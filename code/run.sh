62da4ce9850286f6246e7e05370ac38e404d9859
ArkaRutvik <96034045+demigod-22@users.noreply.github.com>
2022-12-19 15:59:04 +0530

    Update run.sh

--- code/run.sh
+++ code/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=0 python main.py --mode train --dataset lap14 --batch_size 16 --epochs 100 --model_dir savemodel/ --seed 1000 --pooling avg --prefix ../data/D1/
+CUDA_VISIBLE_DEVICES=0 python main.py --mode train --dataset lap14 --batch_size 16 --epochs 100 --model_dir savemodel/ --seed 900 --pooling avg --prefix ../data/D1/
 # CUDA_VISIBLE_DEVICES=0 python main.py --mode test --dataset lap14 --batch_size 16 --epochs 100 --model_dir savemodel/ --seed 1000 --pooling avg --prefix ../data/D2/
